8d01ecf5a8319fbaabb77c910c37df7154c61fde
Mukhammad-amin-ai <aminahmadov123@gmail.com>
2025-06-13 18:19:37 +0500

    [+] auth by yandex added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,40 @@
-import { Link } from 'react-router-dom';
-import assets from '../../assets';
-import './header.scss';
+import { Link } from "react-router-dom";
+import assets from "../../assets";
+import "./header.scss";
 export const Header = () => {
+  const profile = JSON.parse(localStorage.getItem("profile"));
+  const isProfileNotEmpty = profile && Object.keys(profile).length > 0;
+
   return (
-    <header className='header'>
-      <div className='container'>
-        <h3 className='app-name'>Бар Карась</h3>
-        <Link to='/account' className='login-btn'>
-          <img src={assets.userIcon} alt='user icon' />
-          <span>Войти</span>
-        </Link>
-      </div>
+    <header className="header">
+      {!isProfileNotEmpty ? (
+        <div className="container">
+          <h3 className="app-name">Бар Карась</h3>
+          <Link to="/login" className="login-btn">
+            <img src={assets.userIcon} alt="user icon" />
+            <span>Войти</span>
+          </Link>
+        </div>
+      ) : (
+        <div className="container">
+          <div className="account-header">
+            <Link to="/account">
+              <div className="left-box">
+                <div className="img-box">
+                  <img
+                    src={`https://avatars.yandex.net/get-yapic/${profile?.default_avatar_id}/islands-small`}
+                    alt=""
+                  />
+                </div>
+                <span>Мой аккаунт</span>
+              </div>
+            </Link>
+            <a className="email" href="#!">
+              {profile.display_name}
+            </a>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,37 +1,39 @@
-import { Link } from 'react-router-dom';
-import assets from '../../assets';
-import './login.scss';
-import { Button } from '../../components';
+import { Link } from "react-router-dom";
+import assets from "../../assets";
+import "./login.scss";
+import { Button } from "../../components";
+import { openYandexOAuth } from "../../providers/yandex";
+
 export const Login = () => {
   return (
-    <div className='login-page'>
-      <div className='container'>
-        <div className='login-page__content'>
+    <div className="login-page">
+      <div className="container">
+        <div className="login-page__content">
           <img
-            className='company-logo'
+            className="company-logo"
             src={assets.companyLogo}
-            alt='company logo'
+            alt="company logo"
           />
-          <h1 className='login-page__title'>Добро пожаловать</h1>
-          <p className='login-page__desc'>
+          <h1 className="login-page__title">Добро пожаловать</h1>
+          <p className="login-page__desc">
             Чтобы сделать заказ подключитесь к системе через Яндекс
           </p>
         </div>
-        <div className='login-page__btns'>
-          <Link className='yandex-btn' to='#!'>
+        <div className="login-page__btns">
+          <Link onClick={openYandexOAuth} className="yandex-btn" to="#!">
             <img
-              className='yandex-btn__icon'
+              className="yandex-btn__icon"
               src={assets.yandexLogo}
-              alt='yandex logo'
+              alt="yandex logo"
             />
             <span>Войти с Яндекс ID</span>
             <img
-              className='yandex-btn__icon'
+              className="yandex-btn__icon"
               src={assets.user}
-              alt='user img'
+              alt="user img"
             />
           </Link>
-          <Button to='/home' label='Посмотреть меню' grayBtn={true} />
+          <Button to="/home" label="Посмотреть меню" grayBtn={true} />
         </div>
       </div>
     </div>
